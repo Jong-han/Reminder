e9b072fc3f4ecca6c0e3bc8221a66f637d0a8608
Jong-Han <jonghan9611@gmail.com>
2022-03-03 23:40:24 +0900

    Coroutine di module 세팅

---
+++ app/src/main/java/com/jh/reminder/di/CoroutineModule.kt
@@ -0,0 +1,26 @@
+package com.jh.reminder.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+class CoroutineModule {
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+}
